95d100f5d5b5482e98e8417c1b3669759dba0fdf
Trong Nguyen <tn0961350951@gmail.com>
2026-05-07 10:24:28 +0700

    kiểm thử api v2

--- src/main/java/com/example/hrm/shared/controller/HelloController.java
+++ src/main/java/com/example/hrm/shared/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/hello")
+@RequestMapping(value = "/api/v1/hello")
 public class HelloController {
     @GetMapping
     public String hello(){
